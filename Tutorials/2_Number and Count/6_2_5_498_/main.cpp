2e0eb5350ca251e35a12f297354f0c5e0e718be6
jacky <jacky.hsu.tw1@gmail.com>
2022-04-25 00:05:40 +0800

    update

---
+++ Tutorials/2_Number and Count/6_2_5_498_/main.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+
+void SetArray();
+
+int a[1000000];
+
+
+int derivative(int x, int max)
+{
+	long long sum = 0, exp = 1;
+	int i;
+	
+	for(i = max-1; i >= 0 ; i--)
+	{
+		sum += a[i] * exp * (max - i);
+		exp *= x;
+	}
+	return sum;
+}
+
+int main()
+{
+	SetArray();
+	/*
+	int x, n;
+	while(scanf("%d",&x) != EOF)
+	{
+		for(n = 0;; n++)					// user input the element value to array to store
+		{
+			scanf("%d", &a[n]);
+			if(getchar() == '\n'){
+				break;
+			}
+		}
+		
+		printf("%d", derivative(x, n));
+		
+	}	*/
+}
+
+
+int arr[100];
+
+void SetArray()
+{	
+	int n, i;
+	
+	for(n = 0; ; n++)
+	{
+		scanf("%d", &arr[n]);
+		if(getchar() == '\n')
+		{
+			break;
+		}
+	}
+	
+	int size = sizeof(arr)/sizeof(arr[0]);
+	
+	for(int i =0 ; i < 10 ; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
